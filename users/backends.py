82e04664f069f5f00b586baa1481f3ab8f6d2301
Jonas Myrlund <myrlund@gmail.com>
2017-05-06 17:23:55 +0200

    Proper shitty open auth backend implementation.

--- users/backends.py
+++ users/backends.py
@@ -4,7 +4,7 @@ from .models import User
 class OpenBackend(object):
 
     def authenticate(self, request, username=None, password=None):
-        return User.objects.get(username=username)
+        return User.objects.filter(username=username).first()
 
     def get_user(self, user_id):
         return User.objects.filter(pk=user_id).first()
