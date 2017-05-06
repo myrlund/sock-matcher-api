5a0291eb3cbcca2a5c639e2640beb7ba331b6b4d
Jonas Myrlund <myrlund@gmail.com>
2017-05-06 19:12:39 +0200

    Use AWS as storage backend in production.

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -3,6 +3,12 @@ import os
 from .base import *
 
 
+# Prepend prod apps
+INSTALLED_APPS = (
+    'collectfast',
+    's3_folder_storage',
+) + INSTALLED_APPS
+
 DEBUG = False
 
 ALLOWED_HOSTS = [
@@ -10,3 +16,24 @@ ALLOWED_HOSTS = [
 ]
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_BUCKET_NAME')
+
+AWS_LOCATION = 'static/'
+AWS_PRELOAD_METADATA = True
+AWS_S3_FILE_OVERWRITE = False
+
+# Use S3 as file storage backend
+DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
+DEFAULT_S3_PATH = 'media'
+MEDIA_ROOT = '/%s/' % DEFAULT_S3_PATH
+MEDIA_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
+STATIC_S3_PATH = 'static'
+STATIC_ROOT = '/%s/' % STATIC_S3_PATH
+STATIC_URL = 'https://%s.s3.amazonaws.com/static/' % AWS_STORAGE_BUCKET_NAME
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+COMPRESS_URL = STATIC_URL
+COMPRESS_ROOT = STATIC_ROOT
+COMPRESS_STORAGE = STATICFILES_STORAGE
